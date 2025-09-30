172b82961565309033b18e420787eb62b2a81c63
elifafrasanal <elifafrasanal@hotmail.com>
2025-10-01 00:40:37 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyABApwgPetdkl_5Mh9p62SQ83iEZ3iShJk",
-  authDomain: "booktrack-6bdd5.firebaseapp.com",
-  projectId: "booktrack-6bdd5",
-  //storageBucket: "booktrack-6bdd5.firebasestorage.app",
-  storageBucket: "booktrack-6bdd5.appspot.com",
-  messagingSenderId: "735940311580",
-  appId: "1:735940311580:web:79001dc55700c3214cfdd9"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
